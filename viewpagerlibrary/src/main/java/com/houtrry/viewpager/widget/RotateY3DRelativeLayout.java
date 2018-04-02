0a2d51b1a55aad20073cee1ea4cbf2f6e025a67c
houtrry <houtrry@hotmail.com>
2018-04-03 00:11:15 +0800

    添加ViewPager一页显示多条数据的3D旋转效果增强

--- viewpagerlibrary/src/main/java/com/houtrry/viewpager/widget/RotateY3DRelativeLayout.java
+++ viewpagerlibrary/src/main/java/com/houtrry/viewpager/widget/RotateY3DRelativeLayout.java
@@ -50,13 +50,13 @@ public class RotateY3DRelativeLayout extends RelativeLayout {
     private float mMaxRotate = 90;
 
     @Override
-    public void onDraw(Canvas canvas) {
+    public void draw(Canvas canvas) {
 
         super.draw(canvas);
         canvas.save();
 
         mCamera.save(); // 保存 Camera 的状态
-        mCamera.rotateY(mMaxRotate * mProgress); // 旋转 Camera 的三维空间
+        mCamera.rotateY(50); // 旋转 Camera 的三维空间
         canvas.translate(mCenterX, mCenterY); // 旋转之后把投影移动回来
         mCamera.applyToCanvas(canvas); // 把旋转投影到 Canvas
         canvas.translate(-mCenterX, -mCenterY); // 旋转之前把绘制内容移动到轴心（原点）
